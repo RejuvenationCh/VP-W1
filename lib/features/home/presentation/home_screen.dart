37e47a230b0f7145130a45614966446a1bb341be
RejuvenationCh <chrisbrilianto88@gmail.com>
2026-09-17 22:32:55 +0800

    app up : restructed warungapp & homscreen function  into its own file

---
+++ lib/features/home/presentation/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Warung Digital')),
+      body: const Center(
+        child: Text('Belum ada menu'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,9 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() => runApp(const WarungApp());
 
-class WarungApp extends StatelessWidget {
-  const WarungApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Warung Digital',
-      theme: ThemeData(
-        colorSchemeSeed: const Color(0xFF00696E),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Warung Digital')),
-      body: const Center(
-        child: Text('Belum ada menu'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
 
